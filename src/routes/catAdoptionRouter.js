45b589d3f6ec3504649724e90a43d35a7f0bef33
JMBuccolini <96688216+JMBuccolini@users.noreply.github.com>
2022-08-18 21:54:22 -0300

    Ruta para obtener cantidad de cat forms approved

--- src/routes/catAdoptionRouter.js
+++ src/routes/catAdoptionRouter.js
@@ -3,5 +3,6 @@ const catAdoptionRouter = Router();
 const catsFormController = require('../controllers/catsFormController');
 
 catAdoptionRouter.post('/', catsFormController.post);
+catAdoptionRouter.get('/',catsFormController.getApprovedCatAdoption);
 
 module.exports = catAdoptionRouter;
